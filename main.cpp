9590ad3a2a6de2d7bbb7c331502abdcfb6c7eceb
Cristobal Raul Romero Inestroza <crisonic123@hotmail.com>
2016-06-10 18:44:56 -0600

    ya alto

--- main.cpp
+++ main.cpp
@@ -96,50 +96,6 @@ int main(int argc, char const *argv[])
 	{
 		cout<<i<<"-"<<legion[i]->toString()<<endl;
 	}
-	int opcion4=1;
-	int cara1=1,izqui1=1,dere1=1,espal1=1;
-	int cara2=1,izqui2=1,dere2=1,espal2=1;
-	Escuadron c1,i1,d1,e1;
-	Escuadron c2,i2,d2,e2;
-	cout<<"!COMENZANDO SIMULACION!"<<endl;
-	cout<<"(jugador1)ingrese la posicion del Escuadron que desea como el frontal"<<endl;
-	cin>>cara1;
-	cout<<"(jugador1)ingrese la posicion del Escuadron que desea como el de la espalda"<<endl;
-	cin>>espal1;
-	cout<<"(jugador1)ingrese la posicion del Escuadron que desea como el de la izquierda"<<endl;
-	cin>>izqui1;
-	cout<<"(jugador1)ingrese la posicion del Escuadron que desea como el derecho"<<endl;
-	cin>>dere1;
-	cout<<"(jugador2)ingrese la posicion del Escuadron que desea como el frontal"<<endl;
-	cin>>cara2;
-	cout<<"(jugador2)ingrese la posicion del Escuadron que desea como el de la espalda"<<endl;
-	cin>>espal2;
-	cout<<"(jugador2)ingrese la posicion del Escuadron que desea como el de la izquierda"<<endl;
-	cin>>izqui2;
-	cout<<"(jugador2)ingrese la posicion del Escuadron que desea como el derecho"<<endl;
-	cin>>dere2;
-	cout<<"READY, !GO!"
-	while(opcion4!=0){
-		int compro=1
-		while(compro==1){
-			int desi;
-			cout<<"(jugador1)donde desea atacar\n1-cara\n2-espalda\n3-izquierda\n4-derecha"<<endl;
-			cin>>desi;
-			if(desi==1&&cara1!=-1){
-				compro==0;
-			}else if (desi==2&&espal1!=-1)
-			{
-				compro==0;
-			}else if (desi==1&&izqui1!=-1)
-			{
-				compro==0;
-			}else if (desi==1&&dere1!=-1)
-			{
-				compro==0;
-			}else{
-				compro=1;
-			}
-		}			
-	}
+	
 	return 0;
 }
